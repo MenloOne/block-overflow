2be8ad82b4fbf19c29e19609c0517a1a96878578
Rick Bross <rbross3@gmail.com>
2018-10-18 18:10:32 -0400

    add copy confirm msg

--- src/components/AddressTag.tsx
+++ src/components/AddressTag.tsx
@@ -15,6 +15,7 @@ class AddressTagProps {
 interface AddressTagState {
     commandDown: boolean;
     url?: string;
+    statusTip?: string;
 }
 
 export default class AddressTag extends Component<AddressTagProps> {
@@ -25,7 +26,8 @@ export default class AddressTag extends Component<AddressTagProps> {
         super(props, context)
 
         this.state = {
-            commandDown: false
+            commandDown: false,
+            statusTip: ''
         }
 
         this.onKeyPressedDown = this.onKeyPressedDown.bind(this);
@@ -39,17 +41,39 @@ export default class AddressTag extends Component<AddressTagProps> {
 
         document.addEventListener("keydown", this.onKeyPressedDown.bind(this));
         document.addEventListener("keyup", this.onKeyPressedUp.bind(this));
+        document.addEventListener("mousemove", this.onMouseMove.bind(this));
+
     }
 
     componentWillUnmount() {
         document.removeEventListener("keydown", this.onKeyPressedDown.bind(this));
         document.removeEventListener("keyup", this.onKeyPressedUp.bind(this));
+        document.removeEventListener("mousemove", this.onMouseMove.bind(this));
+    }
+
+    onMouseMove(e) {
+        if (e.metaKey) {
+            if (!this.state.commandDown) {
+                this.setState({
+                    commandDown: true,
+                    statusTip: 'Copied!'
+                })
+            }
+        } else {
+            if (this.state.commandDown) {
+                this.setState({
+                    commandDown: false,
+                    statusTip: ''
+                })
+            }
+        }
     }
 
     onKeyPressedDown(e) {
         if (e.keyCode === 91) {
             this.setState({
-                commandDown: true
+                commandDown: true,
+                statusTip: 'Copied!'
             })
         }
     }
@@ -57,22 +81,23 @@ export default class AddressTag extends Component<AddressTagProps> {
     onKeyPressedUp(e) {
         if (e.keyCode === 91) {
             this.setState({
-                commandDown: false
+                commandDown: false,
+                statusTip: ''
             })
         }
     }
 
     onClick(e) {
-
-        if (this.props.link === false) {
-            e.preventDefault(true)
-            return;
-        }
         
         if (this.state.commandDown && this.props.copy !== false) {
             this.copyTextToClipboard(this.props.address)
             e.preventDefault(true)
             return
+        } else {
+            console.log('focus');
+            
+            window.focus();
+            e.stopPropagation();
         }
     }
 
@@ -94,7 +119,7 @@ export default class AddressTag extends Component<AddressTagProps> {
 
         const actionKey = utils.isMacintosh() ? 'Cmd' : 'Ctrl'
         
-        const { url } = this.state
+        const { url, statusTip } = this.state
 
         const renderContents = () => (
             <span className="AddressTag-container">
@@ -102,21 +127,22 @@ export default class AddressTag extends Component<AddressTagProps> {
                     <span className="AddressTag-name-0x">0x</span>
                     <span className="AddressTag-name">{address ? address.slice(2, address.length) : ''}</span>
                     <span className="AddressTag-name-dots">…</span>
+                    <ReactTooltip effect="solid" delayHide={1000} placement='top' event={'focus'} eventOff={'focus'} />
                 </div>
                 {!localStorage.getItem('Tooltip-CtrlClickToCopy') && (
                     <span>
-                        {this.props.copy && <i className="Tooltip-icon" data-tip={`${actionKey}+Click to Copy`}>?</i>}
+                        {this.props.copy && <i className="Tooltip-icon" data-tip={`Click to view on Etherscan, ${actionKey}+Click to Copy`}>?</i>}
                         <ReactTooltip />
                     </span>
                 )}
             </span>)
 
         return this.props.link ? (
-            <a className="AddressTag-link" href={this.props.link ? url : ''} disabled={!this.props.copy && !this.props.link} target="_blank" onClick={(e) => this.onClick(e)}>
+            <a className="AddressTag-link" data-tip={statusTip} href={this.props.link ? url : ''} disabled={!this.props.copy && !this.props.link} target="_blank" onClick={(e) => this.onClick(e)}>
                 {renderContents()}
             </a>
         ) : (
-            <button className="btn-blank AddressTag-link" disabled={!this.props.copy && !this.props.link} onClick={(e) => this.onClick(e)}>
+            <button className="btn-blank AddressTag-link" data-tip={statusTip} disabled={!this.props.copy && !this.props.link} onClick={(e) => this.onClick(e)}>
                 {renderContents()}
             </button>
         )
