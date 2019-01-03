ff2021a326780329f4a26d85e5705c0f589aade4
daviddfm <david@dawson.fm>
2019-01-03 16:47:05 +0100

    Move IPFS URL into config

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,21 @@
 class Config {
     public contentNodeUrl: string
     public apiUrl: string
+    public ipfsProtocol: string
+    public ipfsUrl: string
+    public ipfsPort: string
 
     constructor() {
         if (process.env.NODE_ENV === 'production') {
             this.contentNodeUrl = 'https://cn.menlo.one'
+            this.ipfsProtocol = 'https'
+            this.ipfsUrl  = 'ipfs.menlo.one'
+            this.ipfsPort = '4002'
         } else {
-            this.contentNodeUrl = 'https://cn.menlo.one' // 'http://localhost:8080'
+            this.contentNodeUrl = 'https://cn.menlo.one' // Use 'http://localhost:8080' if running local version of Content Node
+            this.ipfsProtocol = 'https'
+            this.ipfsUrl  = 'ipfs.menlo.one' // Use 'http://localhost' if running local IPFS
+            this.ipfsPort = '4002'
         }
 
         this.apiUrl = `${this.contentNodeUrl}/v0`
